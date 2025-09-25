f04b435daeb0e7911406f4ba64e07a53a96d8d81
Foga <efoger@gmail.com>
2025-09-25 06:49:34 +0300

    тестовый коммит

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,65 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+	"strconv"
+
+	metrics "github.com/Foga2H/go-musthave-metrics/internal/model"
+)
+
+func updateMetrics(res http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Error(res, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	res.Header().Set("content-type", "text/plain")
+
+	typeField := req.PathValue("type")
+	metricField := req.PathValue("metric")
+	valueField := req.PathValue("value")
+
+	if metricField == "" {
+		res.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	if typeField != metrics.Counter && metricField != metrics.Gauge {
+		res.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if typeField == metrics.Counter {
+		if _, err := strconv.ParseInt(valueField, 10, 64); err != nil {
+			res.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	}
+
+	if typeField == metrics.Gauge {
+		if _, err := strconv.ParseFloat(valueField, 64); err != nil {
+			res.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	}
+
+	res.WriteHeader(http.StatusOK)
+}
+
+func notFound(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("content-type", "text/plain")
+	res.WriteHeader(http.StatusNotFound)
+	return
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc(`/update/{type}/{metric}/{value}`, updateMetrics)
+	mux.HandleFunc(`/`, notFound)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
