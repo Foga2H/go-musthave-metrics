a290814da293cb82ed8c7c5f714a077234a6062e
Foga <efoger@gmail.com>
2025-09-26 07:37:41 +0300

    fix: статические фиксы

--- internal/service/agent/sender_service.go
+++ internal/service/agent/sender_service.go
@@ -17,23 +17,39 @@ func NewSenderService() *SenderService {
 
 func (s *SenderService) Send(metrics agent.Metrics) error {
 	for key, value := range metrics.Data {
-		response, err := http.Post(fmt.Sprintf(
-			"http://localhost:8080/update/%s/%s/%s",
+		err := sendRequest(
 			value.Type,
 			key,
 			strconv.FormatFloat(value.Value, 'f', -1, 64),
-		), "text/plain", strings.NewReader(""))
+		)
 
 		if err != nil {
-			panic(err)
-		}
-
-		if response.StatusCode != http.StatusOK {
-			fmt.Printf("Error updating metrics: %s\n", response.Status)
+			return err
 		}
 	}
 
-	fmt.Printf("Successfully updated metrics: %s\n", metrics.Data)
+	fmt.Printf("Successfully updated metrics: %v\n", metrics.Data)
+
+	return nil
+}
+
+func sendRequest(typeField string, nameField string, valueField string) error {
+	response, err := http.Post(fmt.Sprintf(
+		"http://localhost:8080/update/%s/%s/%s",
+		typeField,
+		nameField,
+		valueField,
+	), "text/plain", strings.NewReader(""))
+
+	if err != nil {
+		return err
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		fmt.Printf("Error updating metrics: %s\n", response.Status)
+	}
 
 	return nil
 }
